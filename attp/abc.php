7aa1f0d03ebf68498bad2ed14345b08ebd695dd8
Himanshu-zwt <himanshu@zealousweb.com>
2016-11-24 17:09:56 +0530

    Update abc.php

--- attp/abc.php
+++ attp/abc.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 this is himanshu test
 this is also himanshu test
 this is also himanshu test
 this is also himanshu test
-=======
-this is test.
->>>>>>> 4445da2ba7ec090ca75c363aa8eef445ba4a4d36
+
